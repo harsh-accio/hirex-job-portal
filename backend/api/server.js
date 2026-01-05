e0b90e7f72ebf53dbc6ce4f1d922299c37d22f8b
harsh-accio <rajputharsh9897@gmail.com>
2026-01-06 02:13:12 +0530

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -24,4 +24,4 @@ app.use("/company", companyRoutes);
 app.use("/job", jobRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🌐Server is running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`🌐Server is running on port ${PORT}`));
